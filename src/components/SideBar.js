51d6597dffb1251ef5a02631d3cfdc555cf50432
Matheus Henrique <matthpmoreira@gmail.com>
2024-07-16 16:21:31 -0300

    feat: finish SideBar component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,21 +6,15 @@ export default function SideBar() {
         <div className="sidebar">
             <User />
             <Suggestions />
+
+            <div class="links">
+                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
+                Hashtags • Idioma
+            </div>
+
+            <div class="copyright">
+                © 2021 INSTAGRAM DO FACEBOOK
+            </div>
         </div>
     );
 }
-
-/*
-    <div class="sidebar">
-
-        <div class="links">
-            Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
-            Hashtags • Idioma
-        </div>
-
-        <div class="copyright">
-            © 2021 INSTAGRAM DO FACEBOOK
-        </div>
-    </div>
-
-    */
