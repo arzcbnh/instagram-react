6a64c0dd4c47810b9f87d19b7746b0e763f60462
Matheus Henrique <matthpmoreira@gmail.com>
2024-07-16 15:37:20 -0300

    feat: write NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,49 @@
+export default function NavBar() {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <Logo />
+                <Search />
+                <Icons />
+                <IconsMobile />
+            </div>
+        </div>
+    );
+}
+
+function Logo() {
+    return (
+        <div className="logo">
+            <ion-icon name="logo-instagram"></ion-icon>
+            <div className="separador"></div>
+            <img src="assets/img/logo.png" alt="logo" />
+        </div>
+    );
+}
+
+function Search() {
+    return (
+        <div className="pesquisa">
+            <input type="text" placeholder="Pesquisar" />
+        </div>
+    );
+}
+
+function Icons() {
+    return (
+        <div class="icones">
+            <ion-icon name="paper-plane-outline"></ion-icon>
+            <ion-icon name="compass-outline"></ion-icon>
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="person-outline"></ion-icon>
+        </div>
+    );
+}
+
+function IconsMobile() {
+    return (
+        <div class="icones-mobile">
+            <ion-icon name="paper-plane-outline"></ion-icon>
+        </div>
+    );
+}
