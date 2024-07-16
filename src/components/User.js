e78f2d30b5f42e72d785eb984535fea8ec4e25b5
Matheus Henrique <matthpmoreira@gmail.com>
2024-07-16 16:34:15 -0300

    feat: implement username and avatar changing

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,23 @@
+import React from "react";
+
 export default function User() {
+    const [username, setUsername] = React.useState("catanacomics");
+    const [avatar, setAvatar] = React.useState("assets/img/catanacomics.svg");
+
+    function promptNewData(query, callback) {
+        const newData = prompt(query);
+
+        if (newData)
+            callback(newData);
+    }
+
     return (
         <div className="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img src={avatar} onClick={() => promptNewData("Envie a URL da sua nova imagem de perfil:", setAvatar)} alt="imagem de perfil" />
             <div className="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{username}</strong>
+                    <ion-icon name="pencil" onClick={() => promptNewData("Qual nome de usuário gostaria de usar?", setUsername)}></ion-icon>
                 </span>
             </div>
         </div>
