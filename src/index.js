bae26dbe0d7c803123c3da01d3eb77e655113646
Matheus Henrique <matthpmoreira@gmail.com>
2024-07-16 17:27:26 -0300

    fix: import createRoot from proper module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App.js";
 
 const root = createRoot(document.querySelector(".root"));
